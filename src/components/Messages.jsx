f1dec1cc22dec4ce27bc30ded7c61d064a852cb2
Oleg Gduzii <oleg198679@gmail.com>
2019-02-23 21:41:19 +0300

    Add messages displaying

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const mapStateToProps = ({ messages }) => ({ messages });
+const mapStateToProps = ({ messages }) => ({ ...messages });
 
-// const Messages = ({ allIds, byId }) => (
-const Messages = () => (
-  <p>
-    messages area 2
-  </p>
+const Messages = ({ byId, allIds }) => (
+  allIds.map((id) => {
+    const { user, text } = byId[id];
+    return (
+      <div className="alert alert-light border rounded" role="alert" key={id}>
+        {`${user} said: ${text}`}
+      </div>
+    );
+  })
 );
 
 export default connect(mapStateToProps)(Messages);
